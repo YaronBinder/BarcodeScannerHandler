c8669398af6a6585ad79e8e14cd85c6f1c9f3df4
Yaron Binder <75532643+YaronBinder@users.noreply.github.com>
2024-03-28 09:33:19 +0200

    Add files via upload

---
+++ COMProperties.cs
@@ -0,0 +1,39 @@
+﻿using System.IO.Ports;
+
+namespace ClassRunner.common;
+public class COMProperties( string portName, Parity parity, int baudRate, int dataBits, StopBits stopBits )
+{
+    #region Properties
+
+    /// <summary>
+    /// The name of the com port as shown in the device manager (COM1 or COM2 etc...)
+    /// </summary>
+    public string PortName { get; set; } = portName;
+
+    /// <summary>
+    /// Method of detecting errors in transmission
+    /// </summary>
+    public Parity Parity { get; set; } = parity;
+
+    /// <summary>
+    /// Common unit of measurement of symbol rate
+    /// </summary>
+    public int BaudRate { get; set; } = baudRate;
+
+    /// <summary>
+    /// The number of data bits in each character can be:
+    /// 4 (for Baudot code)
+    /// 5 (rarely used)
+    /// 6 (for true ASCII)
+    /// 7 (for most kinds of data, as this size matches the size of a byte)
+    /// 8 (rarely used)
+    /// </summary>
+    public int DataBits { get; set; } = dataBits;
+
+    /// <summary>
+    /// Sent at the end of every character
+    /// </summary>
+    public StopBits StopBits { get; set; } = stopBits;
+
+    #endregion
+}
---
+++ ScannerManager.cs
@@ -0,0 +1,186 @@
+﻿using IniFiles;
+using DevTools;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows;
+
+namespace ClassRunner.common;
+
+public class ScannerManager : IDisposable
+{
+    #region Constructor
+
+    public ScannerManager() => ActivateAllSerialPorts();
+
+    public ScannerManager( Action<string> scannerAction ) : this() => ScannerAction = scannerAction;
+
+    public ScannerManager( Action<string> scannerAction, string portName )
+    {
+        ScannerAction = scannerAction;
+        ActivateSerialPort( portName );
+    }
+
+    public ScannerManager( Action<string> scannerAction, COMProperties comProperties )
+    {
+        ScannerAction = scannerAction;
+        ActivateSerialPort( comProperties );
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Single instance of open serial port in the device
+    /// </summary>
+    private SerialPort SingalSerialPort { get; set; }
+
+    /// <summary>
+    /// Reading action fired when user scan barcode
+    /// </summary>
+    public Action<string> ScannerAction { get; set; }
+
+    /// <summary>
+    /// List of all seriel ports in the device
+    /// </summary>
+    private List<SerialPort> ScanCodeSerialPort { get; set; }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Activate all serial port in the machine
+    /// </summary>
+    private void ActivateAllSerialPorts()
+    {
+        try
+        {
+            ScanCodeSerialPort = [];
+            foreach ( string portName in SerialPort.GetPortNames() )
+            {
+                try
+                {
+                    SerialPortIniManager spim = new();
+                    ScanCodeSerialPort.Add(
+                        new SerialPort(
+                            portName,
+                            Convert.ToInt32( spim.BaudRate ),
+                            (Parity)Enum.Parse( typeof( Parity ), spim.Parity ),
+                            Convert.ToInt32( spim.DataBits ),
+                            (StopBits)Enum.Parse( typeof( StopBits ), spim.StopBits ) ) );
+                }
+                catch ( Exception e )
+                {
+                    e.WriteLog();
+                }
+            }
+            ScanCodeSerialPort.ForEach( OpenPort );
+        }
+        catch ( Exception e )
+        {
+            e.WriteLog();
+            ScanCodeSerialPort = null;
+        }
+    }
+
+    /// <summary>
+    /// Activate the serial port using given or default port name
+    /// </summary>
+    /// <param name="portName">The port name to be opened</param>
+    private void ActivateSerialPort( string portName )
+    {
+        try
+        {
+            SerialPortIniManager spim = new();
+            SingalSerialPort = new SerialPort(
+                portName ?? spim.CurrentPortName,
+                Convert.ToInt32( spim.BaudRate ),
+                (Parity)Enum.Parse( typeof( Parity ), spim.Parity ),
+                Convert.ToInt32( spim.DataBits ),
+                (StopBits)Enum.Parse( typeof( StopBits ), spim.StopBits ) );
+            OpenPort( SingalSerialPort );
+        }
+        catch ( Exception e )
+        {
+            e.WriteLog();
+            ScanCodeSerialPort = null;
+        }
+    }
+
+    /// <summary>
+    /// Activate the serial port using given COM properties
+    /// </summary>
+    /// <param name="comProp"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    private void ActivateSerialPort( COMProperties comProp )
+    {
+        try
+        {
+            SingalSerialPort = new SerialPort( comProp.PortName,
+                                               comProp.BaudRate,
+                                               comProp.Parity,
+                                               comProp.DataBits,
+                                               comProp.StopBits );
+            OpenPort( SingalSerialPort );
+        }
+        catch ( Exception e )
+        {
+            e.WriteLog();
+            ScanCodeSerialPort = null;
+        }
+    }
+
+    /// <summary>
+    /// Open the newly created serial port
+    /// </summary>
+    /// <param name="serialPort">The serial port to be opened</param>
+    private void OpenPort( SerialPort serialPort )
+    {
+        try
+        {
+            serialPort.DataReceived += DataRecive;
+            if ( !serialPort.IsOpen )
+            {
+                serialPort.Open();
+            }
+        }
+        catch ( Exception e )
+        {
+            e.WriteLog();
+        }
+    }
+
+    private void DataRecive( object serialPort, SerialDataReceivedEventArgs _event )
+        => Dispatch( () => ScannerAction?.Invoke( (serialPort as SerialPort).ReadExisting().Trim() ) );
+
+    public void Close( object sender, EventArgs e ) => Close();
+
+    public void Close() => Dispatch( CloseHandler );
+
+    private void Dispatch( Action action ) => Application.Current.Dispatcher?.Invoke( action );
+
+    private void CloseHandler()
+    {
+        Thread safeClose = new(new ThreadStart(() =>
+        {
+            if (ScanCodeSerialPort?.Count is not null and > 0)
+            {
+                ScanCodeSerialPort.ForEach(port => { if (port?.IsOpen is not null) port.Close(); });
+            }
+            else if (SingalSerialPort?.IsOpen is not null)
+            {
+                SingalSerialPort.Close();
+            }
+        }));
+        safeClose.SetApartmentState( ApartmentState.STA );
+        safeClose.Start();
+        safeClose.Join();
+    }
+
+    public void Dispose() => Close();
+
+    #endregion
+}
